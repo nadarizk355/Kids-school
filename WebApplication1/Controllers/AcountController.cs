f3f70b94a3a2ce3bc864b67e4501204eed0ba545
Options <Options@DESKTOP-2DK2895>
2023-02-19 23:17:25 +0200

    template

--- WebApplication1/Controllers/AcountController.cs
+++ WebApplication1/Controllers/AcountController.cs
@@ -4,6 +4,29 @@ namespace WebApplication1.Controllers
 {
     public class AcountController : Controller
     {
+
+
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+
+
+
         public IActionResult Index()
         {
             return View();
