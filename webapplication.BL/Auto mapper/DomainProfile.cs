8fb860ccbf0f91b1151bc0c04a89a13318026221
Shahira Hegazy <hegazyshahira74@gmail.com>
2023-03-07 14:32:02 +0200

    dashboard

--- webapplication.BL/Auto mapper/DomainProfile.cs
+++ webapplication.BL/Auto mapper/DomainProfile.cs
@@ -1,21 +1,61 @@
-﻿using AutoMapper;
+﻿using AdminDashboard.BLL.Models;
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Transactions;
+using webapplication.BL.Models;
+using webapplication.DAL.entity1;
+using static System.Collections.Specialized.BitVector32;
 
 namespace webapplication.BL.Auto_mapper
 {
     internal class DomainProfile : Profile
     {
-        public DomainProfile()
-        {
-          
-            
-        }
        
+            public DomainProfile()
+            {
+                CreateMap<admin, adminVM>();
+                CreateMap<adminVM, admin>();
+
+                CreateMap<Communications, communicationVM>();
+                CreateMap<communicationVM, Communications>();
+
+                CreateMap<course, courseVM>();
+                CreateMap<courseVM, course>();
+
+                CreateMap<lecturer, lecturerVM>();
+                CreateMap<lecturerVM, lecturer>();
+
+                CreateMap<lectuerstudent, lecturerstudentVM>();
+                CreateMap<lecturerstudentVM, lectuerstudent>();
+
+                CreateMap<parent, parentVM>();
+                CreateMap<parentVM, parent>();
+
+                CreateMap<session, sessionVM>();
+                CreateMap<sessionVM, session>();
+
+                CreateMap<student_cource, student_courseVM>();
+                CreateMap<student_courseVM, student_cource>();
+
+
+                CreateMap<student, studentVM>();
+                CreateMap<studentVM, student>();
+
+                CreateMap<Task, taskVM>();
+                CreateMap<taskVM, Task>();
+
+
+
+            }
+
+
+
+        }
+
 
     }
-}
+
---
+++ webapplication.BL/Models/taskVM.cs
@@ -0,0 +1,28 @@
+﻿using AdminDashboard.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.BLL.Models
+{
+    internal class taskVM
+    {
+        public class tasks
+
+        {
+            public int Id { get; set; }
+            public string title { get; set; }
+            public string linkform { get; set; }
+
+            [ForeignKey("student")]
+            public int Idstudent { get; set; }
+            [ForeignKey("lecture")]
+            public int Idlecture { get; set; }
+            public student student { get; set; }
+            public lecturer lecture { get; set; }
+        }
+    }
+}
