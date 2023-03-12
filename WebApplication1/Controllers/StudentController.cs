231cd1eba7780ef4f171dc01b5faf547859ad306
Options <Options@DESKTOP-2DK2895>
2023-03-13 00:58:16 +0200

    Student

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Student()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Games()
+        {
+            return View();
+        }
+
+
+        public IActionResult Certificate()
+        {
+            return View();
+        }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
+
